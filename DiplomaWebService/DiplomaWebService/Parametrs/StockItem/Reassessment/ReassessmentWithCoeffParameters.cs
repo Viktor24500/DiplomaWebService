dc6a1f181e8e781d0cdb9614c12559c954497743
Viktor Pidhornyy <viktor.podgornyy1@gmail.com>
2025-01-03 16:06:59 +0200

    add stockItem

---
+++ DiplomaWebService/DiplomaWebService/Parametrs/StockItem/Reassessment/ReassessmentWithCoeffParameters.cs
@@ -0,0 +1,14 @@
+﻿namespace DiplomaWebService.Parametrs.StockItem.Reassessment
+{
+	public class ReassessmentWithCoeffParameters
+	{
+		public ReassessmentWithCoeffParameters(decimal oldPrice, decimal coeff)
+		{
+			OldPrice = oldPrice;
+			Coeff = coeff;
+		}
+
+		public decimal OldPrice { get; set; }
+		public decimal Coeff { get; set; }
+	}
+}
---
+++ DiplomaWebService/DiplomaWebService/Parametrs/StockItem/Reassessment/ReassessmentWithoutCoeffParameters.cs
@@ -0,0 +1,14 @@
+﻿namespace DiplomaWebService.Parametrs.StockItem.Reassessment
+{
+	public class ReassessmentWithoutCoeffParameters
+	{
+		public ReassessmentWithoutCoeffParameters(decimal oldPrice, decimal newPrice)
+		{
+			OldPrice = oldPrice;
+			NewPrice = newPrice;
+		}
+
+		public decimal OldPrice { get; set; }
+		public decimal NewPrice { get; set; }
+	}
+}
