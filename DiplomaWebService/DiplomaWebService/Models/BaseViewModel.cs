47206a31be962781c2596b9e2e811dcbe10c394b
Viktor Pidhornyy <viktor.podgornyy1@gmail.com>
2025-02-06 20:58:00 +0200

    fix bugs

---
+++ DiplomaWebService/DiplomaWebService/Models/BaseViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DiplomaWebService.Models
+{
+	public class BaseViewModel
+	{
+		public BaseViewModel(char usernameFirstLetter, string username)
+		{
+			UsernameFirstLetter = usernameFirstLetter;
+			Username = username;
+		}
+		public char UsernameFirstLetter { get; set; }
+		public string Username { get; set; }
+	}
+}
