e1c41e105ba2d6e5a65a7a8218c1b932dfc1d6ed
Viktor Pidhornyy <viktor.podgornyy1@gmail.com>
2025-02-10 19:07:13 +0200

    change UI

--- DiplomaWebService/DiplomaWebService/Models/Contragent.cs
+++ DiplomaWebService/DiplomaWebService/Models/Contragent.cs
@@ -1,22 +1,23 @@
 ﻿namespace DiplomaWebService.Models
 {
-	public class Contragent
-	{
-		public Contragent(int contragentId, int? parentId, string? parentName, string contragentName, bool isActive)
-		{
-			ContragentId = contragentId;
-			ParentId = parentId;
-			ParentName = parentName;
-			ContragentName = contragentName;
-			IsActive = isActive;
+    public class Contragent
+    {
+        public Contragent(int contragentId, int? parentId, string? parentName, string contragentName, bool isActive, string statusDescription)
+        {
+            ContragentId = contragentId;
+            ParentId = parentId;
+            ParentName = parentName;
+            ContragentName = contragentName;
+            IsActive = isActive;
+            StatusDescription = statusDescription;
+        }
 
-		}
+        public int ContragentId { get; set; }
+        public int? ParentId { get; set; }
 
-		public int ContragentId { get; set; }
-		public int? ParentId { get; set; }
-
-		public string? ParentName { get; set; }
-		public string ContragentName { get; set; }
-		public bool IsActive { get; set; }
-	}
+        public string? ParentName { get; set; }
+        public string ContragentName { get; set; }
+        public bool IsActive { get; set; }
+        public string StatusDescription { get; set; }
+    }
 }
